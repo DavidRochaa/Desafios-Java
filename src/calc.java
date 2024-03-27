4070be1a026b12a15cbf30a0c935a86120fa0139
DavidRochaa <davidrochaa.12.11@gmail.com>
2024-03-27 19:59:03 -0300

    Commit 1

---
+++ src/calc.java
@@ -0,0 +1,32 @@
+public class calc {
+
+    float num1;
+    float num2;
+    float result;
+
+    calc(float n1, float n2) {
+        num1 = n1;
+        num2 = n2;
+    }
+
+    void adicao(boolean adicao_bol) {
+        result = num1 + num2;
+        System.out.println("O resultado da adição entre " + num1 + " + " + num2 + " é igual a: " + result);
+    }
+
+    void subtracao(boolean subtracao_bol) {
+        result = num1 - num2;
+        System.out.println("O resultado da subtração entre " + num1 + " + " + num2 + " é igual a: " + result);
+    }
+
+    void multiplicacao(boolean multiplicacao_bol) {
+        result = num1 * num2;
+        System.out.println("O resultado da multiplicação entre " + num1 + " + " + num2 + " é igual a: " + result);
+    }
+
+    void divisao(boolean divisao_bol) {
+        result = num1 / num2;
+        System.out.println("O resultado da divisão entre " + num1 + " + " + num2 + " é igual a: " + result);
+    }
+
+}
